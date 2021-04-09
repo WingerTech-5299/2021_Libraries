88ce08ee185e2cade8dbc297529743c5b1640a3b
Bryce Engberg <bryceengberg@gmail.com>
2021-04-09 12:43:01 -0500

    built driftFixTalonSRX

--- 2021_Libraries/src/main/java/frc/robot/Custom.java
+++ 2021_Libraries/src/main/java/frc/robot/Custom.java
@@ -1,6 +1,10 @@
 package frc.robot;
 
-public final class Custom{
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public final class Custom extends TimedRobot{
 
     public Double getTargetDistance (Double Hight_of_Target, Double Hight_of_camera, double ty){
         Double absTargetHight = Hight_of_Target - Hight_of_camera;
@@ -10,4 +14,23 @@ public final class Custom{
         return targetDistance;
     }
 
+
+    public Double driftFixTalonSRX(TalonSRX leftMotorController, TalonSRX rightMotorController){
+
+         Double driftFix;
+
+         Double skewAmount = Math.abs(leftMotorController.getSelectedSensorPosition()) - Math.abs(rightMotorController.getSelectedSensorPosition());
+
+         if (Math.abs(skewAmount) > 1000){
+             if (skewAmount > 0){
+                 driftFix = -0.3;
+             }else{
+                 driftFix = 0.3;
+             }
+         } else {
+             driftFix = 0.0;
+         }
+
+        return driftFix;
+    }
 }
